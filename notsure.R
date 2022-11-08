690f3c6d814a119248bfb9293bada1db05d85b6b
DOH <82871129+hldohaga@users.noreply.github.com>
2022-11-08 09:30:20 +0000

    Add files via upload

---
+++ notsure.R
@@ -0,0 +1,8 @@
+
+
+ndnsdata_files <- list.files(path = "/media/david/Elements1/contents/DOHmed/MPH/MPHModules/dissertation/alternativequant/altquant/UKDA-6533-spss/spss/spss25", full.names = T, recursive = T)
+ndnspdfs <- list.files(path = "/media/david/Elements1/contents/DOHmed/MPH/MPHModules/dissertation/alternativequant/altquant", full.names = T, recursive = F)
+ndnsgov <- list.files(path = "/media/david/Elements1/contents/DOHmed/MPH/MPHModules/dissertation/alternativequant/altquant/ndns", full.names = T, recursive = F)
+
+
+
